07be529be9fdd23545d72a605dbfd47e368bbd5a
Fatih Eren ÖZÇELİK <eren.ozcelik@std.antalya.edu.tr>
2022-01-17 01:37:29 +0300

    from pc

--- js/02-degisken.js
+++ js/02-degisken.js
@@ -4,7 +4,7 @@ console.log(serverName)
 */
 
 let serverName;
-serverName = "https://kodluyoruz.org"
+serverName = "https://kodluyoruz.org";
 console.log(serverName);
 
 let password = "1234";
@@ -19,7 +19,6 @@ console.log(fullName);
 const serverPassword = "12345";
 //serverPassword = 321;
 console.log(serverPassword);
-
 
 let price = "100";
 console.log(price);
@@ -40,22 +39,21 @@ console.log("username:", isUserName);
 
 console.log(Boolean("11"));
 console.log(Boolean(""));
-// Boolean() içinde veri varsa True, veri yoksa False gösterir. 
+// Boolean() içinde veri varsa True, veri yoksa False gösterir.
 // Boolean(0) -> False  ||  Boolean(0'dan farklı değerler) -> True
 
 // !!!!!!!!! console.log(Boolean(userName.length > 0))
 
-console.log(typeof(isActive));
+console.log(typeof isActive);
 
 let number1 = "20";
-console.log("number1 type:", typeof(number1));
+console.log("number1 type:", typeof number1);
 number1 = parseInt(number1);
-console.log("number1 new type:", typeof(number1));
+console.log("number1 new type:", typeof number1);
 
 // parseInt("11px") -> int ile başlıyorsa sadece int kısmı alıp dönüştürüyor
 // Number("11px") -> ifade int olmadığı için number'a dönüştürmüyor
 
 let number2 = 70;
 number2 = number2.toString();
-console.log("number2 type:", typeof(number2));
-console.log("..")
+console.log("number2 type:", typeof number2);
